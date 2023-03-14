d26cf02feb3f75b1cb759947d16ba031f4aaf814
OdedVaalany <avoded2@gmail.com>
2023-03-14 10:57:35 +0200

    hello

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => res.type('html').send(html));
 
-app.get('/hello', (req, res) => res.type('html').send('Hello there'));
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const html = `
